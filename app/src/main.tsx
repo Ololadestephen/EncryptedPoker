55e45408d9ce89bc04fc49aa515740e414024d6a
Ololade Stephen <stephendirtyworks.com>
2026-02-22 10:25:46 +0100

    Add Vercel Analytics

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom/client';
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
